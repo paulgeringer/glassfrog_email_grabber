cc696159558dc170ab57a08d0038503a8e938b7a
Paul Geringer <paulgeringer@paulgeringer.local>
2013-10-18 16:35:30 -0500

    RoleGrabber now gets all users for each role within a circle

--- lib/role_grabber.rb
+++ lib/role_grabber.rb
@@ -11,21 +11,14 @@ class RoleGrabber
     @circle_hash = circle_hash
   end
 
-  def role_emails( circle, role, hash = circle_hash )
+  def role_member_hash( circle, role, hash = circle_hash )
     circle_id = circle_hash[circle]
     xml = parser.uri_parse( parser.url_generator "circle/#{circle_id}/roles" )
     email_array = emailer.xpath_navigator( emailer.xml_parser( xml ), 'circle/role' )
     role_array = email_array.map do |role|
-      role.xpath('./filled-by').children.xpath('./name')
+      { role: role.xpath('./name').text,
+        people: role.xpath('./filled-by').children.xpath('./id').map {|id| id.text } }
     end
-    role_array 
-    #role_array = [] 
-    #email_array.each do |role|
-      #role_array << role.xpath('filled-by')
-    #end
-    #role_array
-    #emailer.email_map( email_array )
-    #email_array.each do {|role|
-      #role.next_element.next_element.next_elementV`
+    role_array.detect { |hash| hash[:role] == role } 
   end
 end
